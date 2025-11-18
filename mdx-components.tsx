dd570699a7687de07f686816c1b3195805862114
Wahyu Saputro Pratjojo <wahyusaputropratjojo@gmail.com>
2025-11-18 21:31:14 +0800

    feat: add tabs components

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,9 +1,13 @@
+// biome-ignore-all lint: false positive
+
+import * as TabsComponents from "fumadocs-ui/components/tabs";
 import defaultMdxComponents from "fumadocs-ui/mdx";
 import type { MDXComponents } from "mdx/types";
 
 export function getMDXComponents(components?: MDXComponents): MDXComponents {
   return {
     ...defaultMdxComponents,
+    ...TabsComponents,
     ...components,
   };
 }
