41566701f7e23e9c73cac0c2848936cdb39527d5
Wahyu Saputro Pratjojo <wahyusaputropratjojo@gmail.com>
2025-11-18 21:30:51 +0800

    feat: update base oprions configuration

--- lib/layout.shared.tsx
+++ lib/layout.shared.tsx
@@ -2,13 +2,6 @@ import type { BaseLayoutProps } from "fumadocs-ui/layouts/shared";
 
 export function baseOptions(): BaseLayoutProps {
   return {
-    links: [
-      {
-        secondary: false,
-        text: "Documentation",
-        url: "/docs",
-      },
-    ],
     nav: {
       title: "Design System",
     },
