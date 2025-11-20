5434d7a543a31db2a3c1b6331c89f26147161f4d
Wahyu Saputro Pratjojo <wahyusaputropratjojo@gmail.com>
2025-11-20 18:54:59 +0800

    feat: add github url and theme switch mode in base options configuration

--- lib/layout.shared.tsx
+++ lib/layout.shared.tsx
@@ -2,8 +2,12 @@ import type { BaseLayoutProps } from "fumadocs-ui/layouts/shared";
 
 export function baseOptions(): BaseLayoutProps {
   return {
+    githubUrl: "https://github.com/wahyusaputropratjojo/design-system",
     nav: {
       title: "Design System",
+    },
+    themeSwitch: {
+      mode: "light-dark-system",
     },
   };
 }
