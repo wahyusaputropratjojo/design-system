af41f0ba836d1c9fffb770a82496a1f93325474e
Wahyu Saputro Pratjojo <wahyusaputropratjojo@gmail.com>
2025-11-24 20:00:14 +0800

    feat(component): update switch component style

--- components/base/switch.tsx
+++ components/base/switch.tsx
@@ -11,8 +11,8 @@ export function Switch({ ...props }: SwitchProps) {
       className="group relative inline-flex cursor-pointer items-center gap-2 text-label-14 disabled:cursor-not-allowed disabled:brightness-50"
       {...props}
     >
-      <span className="relative isolate inline-flex h-6 w-10 items-center rounded-full border border-gray-400 bg-gray-100 outline-0 outline-transparent outline-offset-2 transition-colors group-focus-visible:z-10 group-focus-visible:outline-3 group-focus-visible:outline-gray-1000 group-selected:border-blue-400 group-selected:bg-blue-700">
-        <span className="relative size-5 translate-x-px rounded-full bg-gray-1000 transition duration-150 ease-in-out group-selected:translate-x-[17px]" />
+      <span className="relative isolate inline-flex h-7 w-16 items-center rounded-full bg-neutral-9 outline-0 outline-transparent outline-offset-2 transition-colors group-focus-visible:z-10 group-focus-visible:outline-3 group-focus-visible:outline-neutral-12 group-selected:border-primary-7 group-selected:bg-primary-9">
+        <span className="relative h-6 w-10 translate-x-0.5 rounded-full bg-white transition duration-150 ease-in-out group-selected:translate-x-[22px]" />
       </span>
     </SwitchPrimitive>
   );
