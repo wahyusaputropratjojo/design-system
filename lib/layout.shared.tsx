8d426101c768a72dc1e6101c19f438db533a4351
Wahyu Saputro Pratjojo <wahyusaputropratjojo@gmail.com>
2025-11-29 20:39:07 +0800

    refactor: apply new biome configuration to entire codebase

--- lib/layout.shared.tsx
+++ lib/layout.shared.tsx
@@ -1,13 +1,13 @@
 import type { BaseLayoutProps } from "fumadocs-ui/layouts/shared";
 
 export function baseOptions(): BaseLayoutProps {
-  return {
-    githubUrl: "https://github.com/wahyusaputropratjojo/design-system",
-    nav: {
-      title: "Design System",
-    },
-    themeSwitch: {
-      mode: "light-dark-system",
-    },
-  };
+	return {
+		githubUrl: "https://github.com/wahyusaputropratjojo/design-system",
+		nav: {
+			title: "Design System",
+		},
+		themeSwitch: {
+			mode: "light-dark-system",
+		},
+	};
 }
--- lib/source.ts
+++ lib/source.ts
@@ -3,15 +3,15 @@ import { type InferPageType, loader } from "fumadocs-core/source";
 import { lucideIconsPlugin } from "fumadocs-core/source/lucide-icons";
 
 export const source = loader({
-  baseUrl: "/",
-  plugins: [lucideIconsPlugin()],
-  source: docs.toFumadocsSource(),
+	baseUrl: "/",
+	plugins: [lucideIconsPlugin()],
+	source: docs.toFumadocsSource(),
 });
 
 export function getPageImage(page: InferPageType<typeof source>) {
-  const segments = [...page.slugs, "image.png"];
-  return {
-    segments,
-    url: `/og/docs/${segments.join("/")}`,
-  };
+	const segments = [...page.slugs, "image.png"];
+	return {
+		segments,
+		url: `/og/docs/${segments.join("/")}`,
+	};
 }
