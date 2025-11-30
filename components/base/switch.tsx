ac182ee8bef2ea1c1b4284123aa3c6e724bb0c19
Wahyu Saputro Pratjojo <wahyusaputropratjojo@gmail.com>
2025-11-30 20:00:36 +0800

    feat(component): add size variant to switch component

--- components/base/switch.tsx
+++ components/base/switch.tsx
@@ -2,18 +2,47 @@
 
 import type { SwitchProps as SwitchPrimitiveProps } from "react-aria-components";
 import { Switch as SwitchPrimitive } from "react-aria-components";
+import type { VariantProps } from "tailwind-variants";
+import { tv } from "@/lib/utils/tailwind-variants";
 
-type SwitchProps = Omit<SwitchPrimitiveProps, "className" | "children">;
+type SwitchProps = Omit<SwitchPrimitiveProps, "className" | "children"> &
+	VariantProps<typeof switchVariants>;
 
-export function Switch({ ...props }: SwitchProps) {
+const switchVariants = tv({
+	base: "group relative isolate inline-flex cursor-pointer items-center gap-2 rounded-full bg-neutral-9 selected:bg-primary-9 text-label-14 outline-0 outline-transparent outline-offset-2 transition-colors focus-visible:z-10 focus-visible:outline-3 focus-visible:outline-neutral-12 disabled:cursor-not-allowed disabled:brightness-50",
+	defaultVariants: {
+		size: "md",
+	},
+	variants: {
+		size: {
+			lg: "h-[28px] w-[64px]",
+			md: "h-[21px] w-[48px]",
+			sm: "h-[14px] w-[32px]",
+		},
+	},
+});
+
+export function Switch({ size, ...props }: SwitchProps) {
 	return (
-		<SwitchPrimitive
-			className="group relative inline-flex cursor-pointer items-center gap-2 text-label-14 disabled:cursor-not-allowed disabled:brightness-50"
-			{...props}
-		>
-			<span className="relative isolate inline-flex h-7 w-16 items-center rounded-full bg-neutral-9 outline-0 outline-transparent outline-offset-2 transition-colors group-focus-visible:z-10 group-focus-visible:outline-3 group-focus-visible:outline-neutral-12 group-selected:border-primary-7 group-selected:bg-primary-9">
-				<span className="relative h-6 w-10 translate-x-0.5 rounded-full bg-white transition duration-150 ease-in-out group-selected:translate-x-[22px]" />
-			</span>
+		<SwitchPrimitive className={switchVariants({ size })} {...props}>
+			<SwitchIndicator size={size} />
 		</SwitchPrimitive>
 	);
+}
+
+const switchIndicatorVariants = tv({
+	base: "relative rounded-full bg-white transition duration-150 ease-in-out",
+	variants: {
+		size: {
+			lg: "h-[24px] w-[40px] translate-x-[2px] group-selected:translate-x-[22px]",
+			md: "h-[18px] w-[30px] translate-x-[1.5px] group-selected:translate-x-[16.5px]",
+			sm: "h-[12px] w-[20px] translate-x-[1px] group-selected:translate-x-[11px]",
+		},
+	},
+});
+
+type SwitchIndicatorProps = VariantProps<typeof switchIndicatorVariants>;
+
+function SwitchIndicator({ size }: SwitchIndicatorProps) {
+	return <span className={switchIndicatorVariants({ size })} />;
 }
