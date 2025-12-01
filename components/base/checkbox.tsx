4c77fc286fc68de00dcd51e996e9c2c5ee62a071
Wahyu Saputro Pratjojo <wahyusaputropratjojo@gmail.com>
2025-12-01 20:43:48 +0800

    feat(component): update checkbox component

--- components/base/checkbox.tsx
+++ components/base/checkbox.tsx
@@ -1,43 +1,41 @@
 "use client";
 
-import { CheckIcon, MinusIcon } from "lucide-react";
 import type { CheckboxProps as CheckboxPrimitiveProps } from "react-aria-components";
 import { Checkbox as CheckboxPrimitive } from "react-aria-components";
 
-export type CheckboxProps = {
-	label?: string;
-	description?: string;
-} & Omit<CheckboxPrimitiveProps, "className" | "children">;
+export type CheckboxProps = Omit<CheckboxPrimitiveProps, "className">;
 
-export function Checkbox({ label, description, ...props }: CheckboxProps) {
+export function Checkbox({ children, ...props }: CheckboxProps) {
 	return (
 		<CheckboxPrimitive
-			className="group relative flex w-fit cursor-pointer gap-3 read-only:cursor-default disabled:cursor-not-allowed disabled:select-none disabled:brightness-50"
+			className="group relative flex w-fit cursor-pointer items-center gap-2 text-label-14 read-only:cursor-default disabled:cursor-not-allowed disabled:select-none disabled:brightness-50"
 			{...props}
 		>
 			{({ isIndeterminate }) => (
 				<>
-					<span className="flex size-5 shrink-0 items-center justify-center rounded-sm border-1 border-gray-400 bg-gray-100 group-indeterminate:bg-gray-1000 group-invalid:border-red-400 group-invalid:bg-red-100 group-focus-visible:border-gray-500 group-focus-visible:outline-2 group-focus-visible:outline-gray-1000 group-focus-visible:outline-offset-2 group-focus-visible:group-invalid:border-red-500 group-selected:bg-gray-1000 group-selected:group-invalid:bg-red-800">
-						{isIndeterminate ? (
-							<MinusIcon
-								aria-hidden="true"
-								className="text-gray-100 group-selected:group-invalid:text-gray-1000"
-							/>
-						) : (
-							<CheckIcon
-								aria-hidden="true"
-								className="hidden text-gray-100 group-selected:block group-selected:group-invalid:text-gray-1000"
-							/>
-						)}
-					</span>
-					{label || description ? (
-						<div className="space-y-1">
-							<p className="text-pretty text-label-16">{label}</p>
-							<p className="text-pretty text-gray-900 text-label-14">
-								{description}
-							</p>
-						</div>
-					) : null}
+					<div className="flex size-4.5 shrink-0 items-center justify-center rounded-md border border-neutral-7 bg-neutral-3 outline-neutral-12 transition-colors group-indeterminate:border-primary-7 group-indeterminate:bg-primary-9 group-invalid:border-danger-7 group-invalid:bg-danger-3 group-focus-visible:outline-2 group-focus-visible:outline-offset-2 group-selected:border-primary-7 group-selected:bg-primary-9 group-selected:group-invalid:border-danger-7 group-selected:group-invalid:bg-danger-9">
+						<svg
+							aria-hidden="true"
+							className="size-3.5 shrink-0 text-white"
+							fill="none"
+							role="graphics-symbol"
+							stroke="currentColor"
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							stroke-width="2"
+							viewBox="0 0 24 24"
+						>
+							{isIndeterminate ? (
+								<path d="M5 12h14" />
+							) : (
+								<path
+									className="opacity-0 transition ease-in-out group-selected:opacity-100"
+									d="M20 6 9 17l-5-5"
+								/>
+							)}
+						</svg>
+					</div>
+					{children}
 				</>
 			)}
 		</CheckboxPrimitive>
