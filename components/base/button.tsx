1428c4b1f61791c2f431dca40ae9128d0f64da25
Wahyu Saputro Pratjojo <wahyusaputropratjojo@gmail.com>
2025-12-24 17:21:39 +0800

    feat(component): update button component

--- components/base/button.tsx
+++ components/base/button.tsx
@@ -1,41 +1,37 @@
 "use client";
 
-import type { ReactNode, Ref } from "react";
+import type { Ref } from "react";
+import type { ButtonProps as ButtonPrimitiveProps } from "react-aria-components";
 import {
   Button as ButtonPrimitive,
-  type ButtonProps as ButtonPrimitiveProps,
+  composeRenderProps,
 } from "react-aria-components";
 import type { VariantProps } from "tailwind-variants";
 import { Spinner } from "@/components/base/spinner";
 import { tv } from "@/lib/utils/tailwind-variants";
 
 const buttonVariants = tv({
-  base: "group relative isolate inline-flex w-fit shrink-0 cursor-pointer pending:cursor-not-allowed select-none items-center justify-center gap-x-(--gap) rounded-full border px-(--padding) outline-0 outline-transparent outline-offset-2 pending:brightness-50 transition-colors focus-visible:z-10 focus-visible:outline-2 focus-visible:outline-neutral-12 disabled:cursor-not-allowed disabled:brightness-50 has-[>[data-slot='suffix']]:pr-[calc(var(--padding)-calc(var(--gap)/2))] has-[>[data-slot='prefix']]:pl-[calc(var(--padding)-calc(var(--gap)/2))] [&_svg]:shrink-0",
+  base: "relative isolate inline-flex cursor-pointer pending:cursor-not-allowed select-none items-center justify-center gap-x-(--gap) rounded-full px-(--padding) pending:opacity-50 outline-0 outline-transparent outline-offset-2 transition-colors focus-visible:z-10 focus-visible:outline-2 focus-visible:outline-neutral-12 disabled:cursor-not-allowed disabled:opacity-50 has-[>svg:last-child]:pr-[calc(var(--padding)-calc(var(--gap)/2))] has-[>svg:first-child]:pl-[calc(var(--padding)-calc(var(--gap)/2))] [&_svg]:size-(--icon-size) [&_svg]:shrink-0",
   compoundVariants: [
     {
       className:
-        "border-danger-7 pressed:border-danger-8 bg-danger-3 pressed:bg-danger-4 text-neutral-12 hover:border-danger-8 hover:bg-danger-4",
+        "bg-danger-3 pressed:bg-danger-4 text-danger-9 hover:bg-danger-4",
       intent: "secondary",
       status: "danger",
     },
     {
-      className: "size-6",
-      iconOnly: true,
-      size: "xs",
-    },
-    {
       className: "size-8",
-      iconOnly: true,
+      isIconOnly: true,
       size: "sm",
     },
     {
       className: "size-10",
-      iconOnly: true,
+      isIconOnly: true,
       size: "md",
     },
     {
       className: "size-12",
-      iconOnly: true,
+      isIconOnly: true,
       size: "lg",
     },
   ],
@@ -47,34 +43,33 @@ const buttonVariants = tv({
     fullWidth: {
       true: "w-full!",
     },
-    iconOnly: {
-      true: "p-0",
-    },
     intent: {
       primary:
-        "border-primary-7 pressed:border-primary-8 bg-primary-9 pressed:bg-primary-10 text-neutral-12 hover:border-primary-8 hover:bg-primary-10",
+        "bg-primary-9 pressed:bg-primary-10 text-light hover:bg-primary-10",
       secondary:
-        "border-neutral-7 pressed:border-neutral-8 bg-neutral-3 pressed:bg-neutral-4 text-neutral-12 hover:border-neutral-8 hover:bg-neutral-4",
+        "bg-neutral-3 pressed:bg-neutral-4 text-primary-9 hover:bg-neutral-4",
+      tertiary:
+        "bg-neutral-3 pressed:bg-neutral-4 text-neutral-12 hover:bg-neutral-4",
+    },
+    isIconOnly: {
+      true: "p-0!",
     },
     size: {
-      lg: "h-12 text-button-16 [--gap:--spacing(2.5)] [--padding:--spacing(5)] [&_svg]:size-5",
-      md: "h-10 text-button-14 [--gap:--spacing(2)] [--padding:--spacing(4)] [&_svg]:size-4",
-      sm: "h-8 text-button-14 [--gap:--spacing(1.5)] [--padding:--spacing(3)] [&_svg]:size-4",
-      xs: "h-6 text-button-12 [--gap:--spacing(1)] [--padding:--spacing(2)] [&_svg]:size-3.5",
+      lg: "h-12 text-label-16 [--gap:--spacing(2.5)] [--icon-size:--spacing(5)] [--padding:--spacing(6)]",
+      md: "h-10 text-label-14 [--gap:--spacing(2)] [--icon-size:--spacing(4)] [--padding:--spacing(5)]",
+      sm: "h-8 text-label-12 [--gap:--spacing(1.5)] [--icon-size:--spacing(4)] [--padding:--spacing(4)]",
     },
     status: {
       danger:
-        "border-danger-7 pressed:border-danger-8 bg-danger-9 pressed:bg-danger-10 text-neutral-12 hover:border-danger-8 hover:bg-danger-10",
+        "border-danger-7 pressed:border-danger-8 bg-danger-9 pressed:bg-danger-10 text-white hover:border-danger-8 hover:bg-danger-10",
     },
   },
 });
 
 type ButtonProps = {
   ref?: Ref<HTMLButtonElement>;
-  suffix?: ReactNode;
-  prefix?: ReactNode;
-  children?: ReactNode;
-} & Omit<ButtonPrimitiveProps, "className" | "children"> &
+  className?: string;
+} & Omit<ButtonPrimitiveProps, "className"> &
   VariantProps<typeof buttonVariants>;
 
 export function Button({
@@ -82,49 +77,30 @@ export function Button({
   ref,
   intent,
   status,
-  iconOnly,
+  isIconOnly,
   fullWidth,
-  suffix,
-  prefix,
-  children,
+  className,
   ...props
 }: ButtonProps) {
   return (
     <ButtonPrimitive
       className={buttonVariants({
+        className,
         fullWidth,
-        iconOnly,
         intent,
+        isIconOnly,
         size,
         status,
       })}
       ref={ref}
       {...props}
     >
-      {({ isPending }) => {
-        if (isPending && iconOnly) {
-          return <Spinner />;
-        }
-
-        if (isPending) {
-          return (
-            <>
-              <span data-slot="prefix">
-                <Spinner />
-              </span>
-              <span data-slot="children">{children}</span>
-            </>
-          );
-        }
-
-        return (
-          <>
-            {prefix && <span data-slot="prefix">{prefix}</span>}
-            <span data-slot="children">{children}</span>
-            {suffix && <span data-slot="suffix">{suffix}</span>}
-          </>
-        );
-      }}
+      {composeRenderProps(props.children, (children, { isPending }) => (
+        <>
+          {isPending && <Spinner />}
+          {children}
+        </>
+      ))}
     </ButtonPrimitive>
   );
 }
