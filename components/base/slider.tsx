aee29bf4597fc111b77a8765abae3a02f9494b26
Wahyu Saputro Pratjojo <wahyusaputropratjojo@gmail.com>
2026-01-12 13:56:47 +0700

    feat: update slider component using base ui dependency

--- components/base/slider.tsx
+++ components/base/slider.tsx
@@ -1,113 +1,17 @@
-"use client";
+import type { SliderRootProps } from "@base-ui/react/slider";
+import { Slider as SliderPrimitive } from "@base-ui/react/slider";
 
-import type { ComponentProps, HTMLAttributes } from "react";
-import { use } from "react";
-import type { SliderProps as SliderPrimitiveProps } from "react-aria-components";
-import {
-  SliderOutput as SliderOutputPrimitive,
-  Slider as SliderPrimitive,
-  SliderStateContext,
-  SliderThumb as SliderThumbPrimitive,
-  SliderTrack as SliderTrackPrimitive,
-} from "react-aria-components";
-import { Label } from "@/components/base/label";
+export type SliderProps = SliderRootProps;
 
-type SliderProps = {
-  label?: string;
-  output?: boolean;
-} & Omit<SliderPrimitiveProps, "className">;
-
-export function Slider({ label, output = true, ...props }: SliderProps) {
+export function Slider(props: SliderProps) {
   return (
-    <SliderPrimitive
-      className="group relative flex orientation-vertical:h-full orientation-vertical:min-h-fit orientation-horizontal:w-full orientation-vertical:w-1.5 orientation-horizontal:min-w-fit touch-none select-none flex-col orientation-vertical:items-center orientation-horizontal:gap-y-3 orientation-vertical:gap-y-3 disabled:brightness-50"
-      data-slot="control"
-      {...props}
-    >
-      <div className="flex items-center justify-between">
-        {label && <Label>{label}</Label>}
-        {output && <SliderOutput />}
-      </div>
-      <SliderTrack>
-        <SliderFill />
-        <SliderThumb />
-      </SliderTrack>
-    </SliderPrimitive>
-  );
-}
-
-type SliderOutputProps = ComponentProps<typeof SliderOutputPrimitive>;
-
-function SliderOutput({ ...props }: SliderOutputProps) {
-  return (
-    <SliderOutputPrimitive
-      className="justify-end place-self-end self-end justify-self-end text-label-14 tabular-nums"
-      {...props}
-    />
-  );
-}
-
-type SliderThumbProps = ComponentProps<typeof SliderThumbPrimitive>;
-
-function SliderThumb({ ...props }: SliderThumbProps) {
-  return (
-    <SliderThumbPrimitive
-      className="top-1/2 left-1/2 rounded-full bg-white shadow-xl focus-visible:outline-3 focus-visible:outline-offset-2 group-orientation-horizontal:h-6 group-orientation-vertical:h-10 group-orientation-horizontal:w-10 group-orientation-vertical:w-6"
-      {...props}
-    />
-  );
-}
-
-type SliderFillProps = Omit<HTMLAttributes<HTMLDivElement>, "className">;
-
-function SliderFill({ ...props }: SliderFillProps) {
-  const state = use(SliderStateContext);
-  const { orientation, values, getThumbValue } = state || {};
-
-  const getFill = () => {
-    const thumbValueA = getThumbValue ? getThumbValue(0) : 0;
-    const thumbValueB = getThumbValue ? getThumbValue(1) : 0;
-
-    if (values?.length === 1) {
-      return orientation === "horizontal"
-        ? {
-            width: `${thumbValueA}%`,
-          }
-        : {
-            height: `${thumbValueA}%`,
-          };
-    }
-
-    return orientation === "horizontal"
-      ? {
-          left: `${thumbValueA}%`,
-          width: `${Math.abs(thumbValueA - thumbValueB)}%`,
-        }
-      : {
-          bottom: `${thumbValueA}%`,
-          height: `${Math.abs(thumbValueA - thumbValueB)}%`,
-        };
-  };
-
-  return (
-    <div
-      className="group-orientation-horizontal/top-0 pointer-events-none absolute rounded-full bg-primary-9 group-disabled/track:opacity-60 group-orientation-vertical/track:bottom-0 group-orientation-horizontal/track:h-full group-orientation-vertical/track:w-full"
-      style={getFill()}
-      {...props}
-    />
-  );
-}
-
-type SliderTrackProps = Omit<
-  ComponentProps<typeof SliderTrackPrimitive>,
-  "className"
->;
-
-export function SliderTrack({ ...props }: SliderTrackProps) {
-  return (
-    <SliderTrackPrimitive
-      className="group/track relative grow rounded-full bg-neutral-9 disabled:opacity-60 group-orientation-vertical:my-5 group-orientation-horizontal:h-1.5 group-orientation-horizontal:w-full group-orientation-vertical:w-1.5 group-orientation-vertical:flex-1"
-      {...props}
-    />
+    <SliderPrimitive.Root thumbAlignment="edge" {...props}>
+      <SliderPrimitive.Control className="flex touch-none select-none items-center">
+        <SliderPrimitive.Track className="h-1.5 w-full select-none rounded-full bg-neutral-9">
+          <SliderPrimitive.Indicator className="select-none rounded-full bg-primary-9" />
+          <SliderPrimitive.Thumb className="h-6 w-10 select-none rounded-full bg-white shadow-black/25 shadow-sm outline-0 outline-transparent transition-colors has-[:focus-visible]:border has-[:focus-visible]:outline-3 has-[:focus-visible]:outline-primary-9 has-[:focus-visible]:outline-offset-2" />
+        </SliderPrimitive.Track>
+      </SliderPrimitive.Control>
+    </SliderPrimitive.Root>
   );
 }
