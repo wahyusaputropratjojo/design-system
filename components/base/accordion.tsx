72adcc39de5c79904d824b1044f6c33219a39205
Wahyu Saputro Pratjojo <wahyusaputropratjojo@gmail.com>
2026-01-14 19:16:37 +0800

    feat(component): update accordion component style

--- components/base/accordion.tsx
+++ components/base/accordion.tsx
@@ -6,39 +6,46 @@ import type {
   AccordionTriggerProps as AccordionTriggerBaseProps,
 } from "@base-ui/react/accordion";
 import { Accordion as AccordionBase } from "@base-ui/react/accordion";
+import { cn } from "@/lib/utils/tailwind-variants"
 
-type AccordionProps = AccordionRootBaseProps;
+type AccordionProps = { className: string } & Omit<AccordionRootBaseProps, "className">;
 
-export function Accordion(props: AccordionProps) {
+export function Accordion({ className, ...props }: AccordionProps) {
   return (
     <AccordionBase.Root
-      className="flex w-96 max-w-[calc(100vw-8rem)] flex-col divide-y divide-neutral-6"
+      className={cn("flex flex-col divide-y divide-neutral-6", className)}
       {...props}
     />
   );
 }
 
-type AccordionItemProps = AccordionItemBaseProps;
+type AccordionItemProps = { className: string } & Omit<AccordionItemBaseProps, "className">;
 
 export function AccordionItem({ className, ...props }: AccordionItemProps) {
-  return <AccordionBase.Item {...props} />;
+  return (
+    <AccordionBase.Item
+      className={cn("group/item bg-neutral-3 first:rounded-t-2xl last:rounded-b-2xl", className)}
+      {...props}
+    />
+  );
 }
 
-type AccordionHeaderProps = AccordionHeaderBaseProps;
+type AccordionHeaderProps = { className: string } & Omit<AccordionHeaderBaseProps, "className">;
 
-export function AccordionHeader(props: AccordionHeaderProps) {
-  return <AccordionBase.Header className="m-0!" {...props} />;
+export function AccordionHeader({ className, ...props }: AccordionHeaderProps) {
+  return <AccordionBase.Header className={cn("not-prose", className)} {...props} />;
 }
 
-type AccordionTriggerProps = AccordionTriggerBaseProps;
+type AccordionTriggerProps = { className: string } & Omit<AccordionTriggerBaseProps, "className">;
 
 export function AccordionTrigger({
+  className,
   children,
   ...props
 }: AccordionTriggerProps) {
   return (
     <AccordionBase.Trigger
-      className="group relative flex w-full cursor-pointer items-center justify-between gap-4 p-3 text-label-14 outline-0 outline-transparent transition-color transition-colors hover:bg-neutral-4 focus-visible:z-1 focus-visible:outline-2 focus-visible:outline-primary-9 focus-visible:outline-offset-2"
+      className={cn("group relative flex w-full cursor-pointer items-center justify-between gap-4 p-3 text-label-14 outline-0 outline-transparent hover:bg-neutral-4 focus-visible:z-1 focus-visible:outline-2 focus-visible:outline-primary-9 focus-visible:outline-offset-2 group-first/item:rounded-t-2xl group-last/item:rounded-b-2xl data-panel-open:hover:bg-transparent", className)}
       {...props}
     >
       {children}
@@ -58,12 +65,12 @@ export function AccordionTrigger({
   );
 }
 
-type AccordionPanelProps = AccordionPanelBaseProps;
+type AccordionPanelProps = { className: string } & Omit<AccordionPanelBaseProps, "className">;
 
-export function AccordionPanel(props: AccordionPanelProps) {
+export function AccordionPanel({ className, ...props }: AccordionPanelProps) {
   return (
     <AccordionBase.Panel
-      className="p-3 text-neutral-11 text-paragraph-14"
+      className={cn("overflow-hidden px-3 pt-0 pb-3 text-neutral-11 text-paragraph-14", className)}
       {...props}
     />
   );
