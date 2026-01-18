c1e54a4c97afdd69da240ecb528aaa65db6fe35d
Wahyu Saputro Pratjojo <wahyusaputropratjojo@gmail.com>
2026-01-18 20:41:13 +0800

    feat(component): change background color in preview card component

--- components/preview-card.tsx
+++ components/preview-card.tsx
@@ -5,7 +5,7 @@ type PreviewCard = ComponentProps<"section">;
 export function PreviewCard({ ...props }: PreviewCard) {
   return (
     <section
-      className="flex min-h-80 flex-col items-center justify-center gap-2 rounded-lg border bg-fd-card sm:flex-row"
+      className="flex min-h-80 flex-col items-center justify-center gap-2 rounded-lg border bg-neutral-1 sm:flex-row"
       {...props}
     />
   );
