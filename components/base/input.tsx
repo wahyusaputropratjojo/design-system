1b5b10d90404cc56c3585f80182ae5122d62af77
Wahyu Saputro Pratjojo <wahyusaputropratjojo@gmail.com>
2026-01-18 20:18:51 +0800

    feat(component): add input component

---
+++ components/base/input.tsx
@@ -0,0 +1,19 @@
+import type { InputProps as InputBaseProps } from "@base-ui/react/input";
+import { Input as InputBase } from "@base-ui/react/input";
+import { cn } from "@/lib/utils/tailwind-variants";
+
+type InputProps = {
+  className: string;
+} & Omit<InputBaseProps, "className">;
+
+export function Input({ className, ...props }: InputProps) {
+  return (
+    <InputBase
+      className={cn(
+        "h-10 rounded-xl border border-neutral-6 bg-neutral-3 p-3 text-paragraph-14 outline-0 outline-transparent outline-offset-2 transition-colors focus:outline-3 focus:outline-primary-8",
+        className,
+      )}
+      {...props}
+    />
+  );
+}
