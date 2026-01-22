b2219bab43d744e6f00b905489fe34b93fe3cdbc
Wahyu Saputro Pratjojo <wahyusaputropratjojo@gmail.com>
2026-01-22 22:44:32 +0800

    feat(component): update separator component

--- components/base/separator.tsx
+++ components/base/separator.tsx
@@ -4,8 +4,19 @@ import { cn } from "@/lib/utils/tailwind-variants";
 
 export type SeparatorProps = ComponentProps<typeof SeparatorBase>;
 
-export function Separator({ className, ...props }: SeparatorProps) {
+export function Separator({
+  className,
+  orientation = "horizontal",
+  ...props
+}: SeparatorProps) {
   return (
-    <SeparatorBase className={cn("w-px bg-neutral-6", className)} {...props} />
+    <SeparatorBase
+      className={cn(
+        "shrink-0 bg-neutral-6 data-[orientation=horizontal]:h-px data-[orientation=horizontal]:w-full data-[orientation=vertical]:w-px data-[orientation=vertical]:not-[[class^='h-']]:not-[[class*='_h-']]:self-stretch",
+        className,
+      )}
+      orientation={orientation}
+      {...props}
+    />
   );
 }
