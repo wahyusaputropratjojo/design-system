dce405e0620f0ea8506105855b0c7df226b47d9a
Wahyu Saputro Pratjojo <wahyusaputropratjojo@gmail.com>
2026-01-22 15:21:04 +0800

    feat(component): update select component

--- components/base/select.tsx
+++ components/base/select.tsx
@@ -1,4 +1,10 @@
 import { Select as SelectBase } from "@base-ui/react/select";
+import {
+  CheckIcon,
+  ChevronDownIcon,
+  ChevronsUpDownIcon,
+  ChevronUpIcon,
+} from "lucide-react";
 import type { ComponentProps } from "react";
 import { cn } from "@/lib/utils/tailwind-variants";
 
@@ -10,15 +16,25 @@ export function Select(props: SelectProps) {
 
 export type SelectTriggerProps = ComponentProps<typeof SelectBase.Trigger>;
 
-export function SelectTrigger({ className, ...props }: SelectTriggerProps) {
+export function SelectTrigger({
+  children,
+  className,
+  ...props
+}: SelectTriggerProps) {
   return (
     <SelectBase.Trigger
       className={cn(
-        "flex h-10 min-w-40 select-none items-center justify-between rounded-xl bg-neutral-3 px-3 text-paragraph-14 outline-transparent transition-colors hover:bg-neutral-4 focus-visible:outline-3 focus-visible:outline-primary-8 focus-visible:outline-offset-2 data-disabled:cursor-not-allowed data-popup-open:bg-neutral-5 data-disabled:opacity-50",
+        "flex h-10 min-w-60 cursor-pointer select-none items-center justify-between rounded-xl bg-neutral-3 px-3 text-paragraph-14 outline-transparent transition-colors hover:not-disabled:bg-neutral-4 focus-visible:outline-3 focus-visible:outline-primary-8 focus-visible:outline-offset-2 data-disabled:cursor-not-allowed data-popup-open:bg-neutral-5 data-disabled:opacity-50",
         className,
       )}
+      data-slot="select-trigger"
       {...props}
-    />
+    >
+      {children}
+      <SelectBase.Icon data-slot="select-icon">
+        <ChevronsUpDownIcon className="size-3.5 shrink-0" />
+      </SelectBase.Icon>
+    </SelectBase.Trigger>
   );
 }
 
@@ -31,226 +47,110 @@ export function SelectValue({ className, ...props }: SelectValueProps) {
         "text-neutral-12 data-placeholder:text-neutral-11",
         className,
       )}
+      data-slot="select-value"
       {...props}
     />
   );
 }
 
-export type SelectIconProps = ComponentProps<typeof SelectBase.Icon>;
+export type SelectPopupProps = ComponentProps<typeof SelectBase.Popup> &
+  Pick<
+    ComponentProps<typeof SelectBase.Positioner>,
+    "alignItemWithTrigger" | "sideOffset"
+  >;
 
-export function SelectIcon({ className, ...props }: SelectIconProps) {
-  return (
-    <SelectBase.Icon className={cn("", className)} {...props}>
-      <svg
-        className="size-3.5"
-        fill="none"
-        height="24"
-        stroke="currentColor"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        strokeWidth="2"
-        viewBox="0 0 24 24"
-        width="24"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <title>Chevron Up Down Icon</title>
-        <path d="m7 15 5 5 5-5" />
-        <path d="m7 9 5-5 5 5" />
-      </svg>
-    </SelectBase.Icon>
-  );
-}
-
-export type SelectPortalProps = ComponentProps<typeof SelectBase.Portal>;
-
-export function SelectPortal(props: SelectProps) {
-  return <SelectBase.Portal {...props} />;
-}
-
-export type SelectPositionerProps = ComponentProps<
-  typeof SelectBase.Positioner
->;
-
-export function SelectPositioner({
+export function SelectPopup({
+  children,
+  alignItemWithTrigger = true,
+  sideOffset = 8,
   className,
   ...props
-}: SelectPositionerProps) {
+}: SelectPopupProps) {
   return (
-    <SelectBase.Positioner
-      className={cn("", className)}
-      sideOffset={8}
-      {...props}
-    />
-  );
-}
-
-export type SelectPopupProps = ComponentProps<typeof SelectBase.Popup>;
-
-export function SelectPopup({ className, ...props }: SelectPopupProps) {
-  return (
-    <SelectBase.Popup
-      className={cn(
-        "min-w-(--anchor-width) rounded-xl bg-neutral-3",
-        className,
-      )}
-      {...props}
-    />
-  );
-}
-
-export type SelectListProps = ComponentProps<typeof SelectBase.List>;
-
-export function SelectList({ className, ...props }: SelectListProps) {
-  return (
-    <SelectBase.List
-      className={cn(
-        "relative max-h-60 overflow-clip overflow-y-auto rounded-xl outline-0",
-        className,
-      )}
-      {...props}
-    />
-  );
-}
-
-export type SelectScrollUpArrowProps = ComponentProps<
-  typeof SelectBase.ScrollUpArrow
->;
-
-export function SelectScrollUpArrow({
-  className,
-  ...props
-}: SelectScrollUpArrowProps) {
-  return (
-    <SelectBase.ScrollUpArrow
-      className={cn(
-        "top-0 z-1 flex h-5 w-full items-center justify-center rounded-t-xl bg-neutral-5 text-neutral-12",
-        className,
-      )}
-      {...props}
-    >
-      <svg
-        className="size-3.5 shrink-0"
-        fill="none"
-        height="24"
-        stroke="currentColor"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        strokeWidth="2"
-        viewBox="0 0 24 24"
-        width="24"
-        xmlns="http://www.w3.org/2000/svg"
+    <SelectBase.Portal data-slot="select-portal">
+      <SelectBase.Positioner
+        alignItemWithTrigger={alignItemWithTrigger}
+        data-slot="select-positioner"
+        sideOffset={sideOffset}
       >
-        <title>Chevron Up Icon</title>
-        <path d="m18 15-6-6-6 6" />
-      </svg>
-    </SelectBase.ScrollUpArrow>
+        <SelectBase.ScrollUpArrow
+          className="top-0 z-1 flex h-3.5 w-full items-center justify-center rounded-t-lg bg-neutral-5 text-neutral-12"
+          data-slot="select-scroll-up-arrow"
+        >
+          <ChevronUpIcon className="size-3 shrink-0" />
+        </SelectBase.ScrollUpArrow>
+        <SelectBase.Popup
+          className={cn(
+            "min-w-(--anchor-width) origin-(--transform-origin) cursor-pointer rounded-xl bg-neutral-3 p-1 outline-0",
+            className,
+          )}
+          {...props}
+        >
+          <SelectBase.List
+            className="flex max-h-60 flex-col gap-1 overflow-y-auto"
+            data-slot="select-list"
+          >
+            {children}
+          </SelectBase.List>
+        </SelectBase.Popup>
+        <SelectBase.ScrollDownArrow
+          className="bottom-0 z-1 flex h-3.5 w-full items-center justify-center rounded-b-xl bg-neutral-5 text-neutral-12"
+          data-slot="select-scroll-down-arrow"
+        >
+          <ChevronDownIcon className="size-3 shrink-0" />
+        </SelectBase.ScrollDownArrow>
+      </SelectBase.Positioner>
+    </SelectBase.Portal>
   );
-}
-
-export type SelectScrollDownArrowProps = ComponentProps<
-  typeof SelectBase.ScrollDownArrow
->;
-
-export function SelectScrollDownArrow({
-  className,
-  ...props
-}: SelectScrollDownArrowProps) {
-  return (
-    <SelectBase.ScrollDownArrow
-      className={cn(
-        "bottom-0 z-1 flex h-5 w-full items-center justify-center rounded-b-xl bg-neutral-5 text-neutral-12",
-        className,
-      )}
-      {...props}
-    >
-      <svg
-        className="size-3.5 shrink-0"
-        fill="none"
-        height="24"
-        stroke="currentColor"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        strokeWidth="2"
-        viewBox="0 0 24 24"
-        width="24"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <title>Chevron Down Icon</title>
-        <path d="m6 9 6 6 6-6" />
-      </svg>
-    </SelectBase.ScrollDownArrow>
-  );
-}
-
-export type SelectBackdropProps = ComponentProps<typeof SelectBase.Backdrop>;
-
-export function SelectBackdrop({ className, ...props }: SelectBackdropProps) {
-  return <SelectBase.Backdrop className={cn("", className)} {...props} />;
 }
 
 export type SelectItemProps = ComponentProps<typeof SelectBase.Item>;
 
-export function SelectItem({ className, ...props }: SelectItemProps) {
+export function SelectItem({ children, className, ...props }: SelectItemProps) {
   return (
     <SelectBase.Item
       className={cn(
-        "flex h-10 items-center justify-between px-3 text-neutral-11 outline-0 first:rounded-t-xl last:rounded-b-xl data-highlighted:bg-neutral-4 data-selected:bg-primary-5 data-selected:text-primary-12",
+        "flex h-8 cursor-pointer items-center justify-between rounded-lg px-2 text-neutral-11 outline-0 data-highlighted:bg-neutral-4 data-selected:bg-primary-5 data-selected:text-primary-12",
         className,
       )}
+      data-slot="select-item"
       {...props}
-    />
-  );
-}
-
-export type SelectItemTextProps = ComponentProps<typeof SelectBase.ItemText>;
-
-export function SelectItemText({ className, ...props }: SelectItemTextProps) {
-  return (
-    <SelectBase.ItemText
-      className={cn("select-none text-paragraph-14", className)}
-      {...props}
-    />
-  );
-}
-
-export type SelectItemIndicatorProps = ComponentProps<
-  typeof SelectBase.ItemIndicator
->;
-
-export function SelectItemIndicator({
-  className,
-  ...props
-}: SelectItemIndicatorProps) {
-  return (
-    <SelectBase.ItemIndicator className={cn("", className)} {...props}>
-      <svg
-        fill="none"
-        height="24"
-        stroke="currentColor"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        strokeWidth="2"
-        viewBox="0 0 24 24"
-        width="24"
-        xmlns="http://www.w3.org/2000/svg"
+    >
+      <SelectBase.ItemText
+        className="select-none text-paragraph-14"
+        data-slot="select-item-text"
       >
-        <title>Check Icon</title>
-        <path d="M20 6 9 17l-5-5" />
-      </svg>
-    </SelectBase.ItemIndicator>
+        {children}
+      </SelectBase.ItemText>
+      <SelectBase.ItemIndicator data-slot="select-item-indicator">
+        <CheckIcon className="size-3.5 shrink-0" />
+      </SelectBase.ItemIndicator>
+    </SelectBase.Item>
   );
 }
 
 export type SelectSeparatorProps = ComponentProps<typeof SelectBase.Separator>;
 
 export function SelectSeparator({ className, ...props }: SelectSeparatorProps) {
-  return <SelectBase.Separator className={cn("", className)} {...props} />;
+  return (
+    <SelectBase.Separator
+      className={cn("my-1.5 h-px shrink-0 bg-neutral-6", className)}
+      data-slot="select-separator"
+      {...props}
+    />
+  );
 }
 
 export type SelectGroupProps = ComponentProps<typeof SelectBase.Group>;
 
 export function SelectGroup({ className, ...props }: SelectGroupProps) {
-  return <SelectBase.Group className={cn("", className)} {...props} />;
+  return (
+    <SelectBase.Group
+      className={cn("flex flex-col gap-1", className)}
+      data-slot="select-group"
+      {...props}
+    />
+  );
 }
 
 export type SelectGroupLabelProps = ComponentProps<
@@ -261,5 +161,14 @@ export function SelectGroupLabel({
   className,
   ...props
 }: SelectGroupLabelProps) {
-  return <SelectBase.GroupLabel className={cn("", className)} {...props} />;
+  return (
+    <SelectBase.GroupLabel
+      className={cn(
+        "rounded-full px-2 pt-2 pb-1 text-label-10 text-neutral-12",
+        className,
+      )}
+      data-slot="select-group-label"
+      {...props}
+    />
+  );
 }
