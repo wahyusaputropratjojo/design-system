c8ed5bb5c866627dae970284fda93e23badbbbf3
Wahyu Saputro Pratjojo <wahyusaputropratjojo@gmail.com>
2026-01-25 20:58:42 +0800

    feat(component): use base-ui useRender instead of plain span element in badge component

--- components/base/badge.tsx
+++ components/base/badge.tsx
@@ -1,31 +1,41 @@
-import type { ComponentProps } from "react";
+import { mergeProps } from "@base-ui/react/merge-props";
+import { useRender } from "@base-ui/react/use-render";
 import type { VariantProps } from "tailwind-variants";
 import { tv } from "@/lib/utils/tailwind-variants";
 
-const badgeVariants = tv({
-  base: "inline-flex select-none items-center rounded-full px-2 py-1 text-label-12",
+const badge = tv({
+  base: "inline-flex h-6 select-none items-center gap-1 rounded-full px-2 text-label-12 no-underline transition-colors [&>svg]:size-3!",
   defaultVariants: {
     intent: "primary",
   },
   variants: {
     intent: {
-      primary: "bg-primary-3 text-primary-9",
+      primary: "bg-primary-3 text-primary-12",
       secondary: "bg-neutral-3 text-neutral-12",
     },
   },
 });
 
-export type BadgeProps = ComponentProps<"span"> &
-  VariantProps<typeof badgeVariants>;
+interface BadgeProps
+  extends useRender.ComponentProps<"span">,
+    VariantProps<typeof badge> {}
 
-export function Badge({ className, intent, ...props }: BadgeProps) {
-  return (
-    <span
-      className={badgeVariants({
-        className,
-        intent,
-      })}
-      {...props}
-    />
-  );
+export function Badge({ className, intent, render, ...props }: BadgeProps) {
+  return useRender({
+    defaultTagName: "span",
+    props: mergeProps<"span">(
+      {
+        className: badge({
+          className,
+          intent,
+        }),
+      },
+      props,
+    ),
+    render,
+    state: {
+      intent,
+      slot: "badge",
+    },
+  });
 }
