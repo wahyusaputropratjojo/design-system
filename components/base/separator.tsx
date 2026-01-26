f7b0ee3cfa4af07e486b9a07c1b786a8747fe3b2
Wahyu Saputro Pratjojo <wahyusaputropratjojo@gmail.com>
2026-01-26 19:44:11 +0800

    refactor(component): rename base-ui import aliases in separator component

--- components/base/separator.tsx
+++ components/base/separator.tsx
@@ -1,16 +1,13 @@
-import { Separator as SeparatorBase } from "@base-ui/react/separator";
-import type { ComponentProps } from "react";
+import { Separator as SeparatorPrimitive } from "@base-ui/react/separator";
 import { cn } from "@/lib/utils/tailwind-variants";
-
-export type SeparatorProps = ComponentProps<typeof SeparatorBase>;
 
 export function Separator({
   className,
   orientation = "horizontal",
   ...props
-}: SeparatorProps) {
+}: SeparatorPrimitive.Props) {
   return (
-    <SeparatorBase
+    <SeparatorPrimitive
       className={cn(
         "shrink-0 bg-neutral-6 data-[orientation=horizontal]:h-px data-[orientation=horizontal]:w-full data-[orientation=vertical]:w-px data-[orientation=vertical]:not-[[class^='h-']]:not-[[class*='_h-']]:self-stretch",
         className,
