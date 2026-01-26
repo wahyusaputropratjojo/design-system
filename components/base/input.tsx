f343a217a5190257d321bb232bfca09b29ef5a03
Wahyu Saputro Pratjojo <wahyusaputropratjojo@gmail.com>
2026-01-26 19:41:30 +0800

    refactor(component): rename base-ui import aliases in input component

--- components/base/input.tsx
+++ components/base/input.tsx
@@ -1,12 +1,9 @@
-import { Input as InputBase } from "@base-ui/react/input";
-import type { ComponentProps } from "react";
+import { Input as InputPrimitive } from "@base-ui/react/input";
 import { cn } from "@/lib/utils/tailwind-variants";
 
-type InputProps = ComponentProps<typeof InputBase>;
-
-export function Input({ className, ...props }: InputProps) {
+export function Input({ className, ...props }: InputPrimitive.Props) {
   return (
-    <InputBase
+    <InputPrimitive
       className={cn(
         "h-10 rounded-xl border border-neutral-6 bg-neutral-3 p-3 text-paragraph-14 outline-0 outline-transparent outline-offset-2 transition-colors focus:outline-3 focus:outline-primary-8",
         className,
