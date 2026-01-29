4d353b8ac3beb9828f57af43e537b3acc31ffac7
Wahyu Saputro Pratjojo <wahyusaputropratjojo@gmail.com>
2026-01-29 17:23:02 +0800

    feat(component): update preview card component

--- components/preview-card.tsx
+++ components/preview-card.tsx
@@ -6,7 +6,7 @@ export function PreviewCard({ className, ...props }: PreviewCardProps) {
   return (
     <section
       className={cn(
-        "not-prose flex min-h-100 flex-col items-center justify-center gap-2 rounded-xl border border-fd-accent bg-fd-card p-16 shadow-sm sm:flex-row",
+        "not-prose flex min-h-100 flex-col items-center justify-center gap-2 rounded-xl border border-fd-accent bg-fd-card p-4 shadow-sm sm:flex-row sm:p-16",
         className,
       )}
       {...props}
