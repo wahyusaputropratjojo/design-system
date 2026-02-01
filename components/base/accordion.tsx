1d95e96af613f726be199caf2a988d2e25d8254b
Wahyu Saputro Pratjojo <wahyusaputropratjojo@gmail.com>
2026-02-01 21:32:50 +0800

    feat(component): update accordion component and replace tailwindcss with css modules

--- components/base/accordion.tsx
+++ components/base/accordion.tsx
@@ -1,6 +1,6 @@
 import { Accordion as AccordionPrimitive } from "@base-ui/react/accordion";
 import { ChevronDownIcon } from "lucide-react";
-import { cn } from "@/lib/utils/tailwind-variants";
+import styles from "./accordion.module.css";
 
 export function Accordion({
   className,
@@ -8,7 +8,7 @@ export function Accordion({
 }: AccordionPrimitive.Root.Props) {
   return (
     <AccordionPrimitive.Root
-      className={cn("flex flex-col divide-y divide-neutral-6", className)}
+      className={styles.root}
       data-slot="accordion"
       {...props}
     />
@@ -21,7 +21,7 @@ export function AccordionHeader({
 }: AccordionPrimitive.Header.Props) {
   return (
     <AccordionPrimitive.Header
-      className={cn("not-prose", className)}
+      className={styles.header}
       data-slot="accordion-header"
       {...props}
     />
@@ -34,10 +34,7 @@ export function AccordionItem({
 }: AccordionPrimitive.Item.Props) {
   return (
     <AccordionPrimitive.Item
-      className={cn(
-        "group/item bg-neutral-3 first:rounded-t-2xl last:rounded-b-2xl",
-        className,
-      )}
+      className={styles.item}
       data-slot="accordion-item"
       {...props}
     />
@@ -50,10 +47,7 @@ export function AccordionPanel({
 }: AccordionPrimitive.Panel.Props) {
   return (
     <AccordionPrimitive.Panel
-      className={cn(
-        "overflow-hidden px-3 pt-0 pb-3 text-neutral-11 text-paragraph-14",
-        className,
-      )}
+      className={styles.panel}
       data-slot="accordion-panel"
       {...props}
     />
@@ -67,15 +61,12 @@ export function AccordionTrigger({
 }: AccordionPrimitive.Trigger.Props) {
   return (
     <AccordionPrimitive.Trigger
-      className={cn(
-        "group relative flex w-full cursor-pointer items-center justify-between gap-4 p-3 text-label-14 outline-0 outline-transparent hover:bg-neutral-4 focus-visible:z-1 focus-visible:outline-3 focus-visible:outline-primary-8 group-first/item:rounded-t-2xl group-last/item:rounded-b-2xl data-panel-open:hover:bg-transparent",
-        className,
-      )}
+      className={styles.trigger}
       data-slot="accordion-trigger"
       {...props}
     >
       {children}
-      <ChevronDownIcon className="size-4 shrink-0 stroke-2 transition-all ease-out group-data-panel-open:-rotate-180" />
+      <ChevronDownIcon />
     </AccordionPrimitive.Trigger>
   );
 }
