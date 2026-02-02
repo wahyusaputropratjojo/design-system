c42d1459da7756fb734195ecaa02694ca5d65bcf
Wahyu Saputro Pratjojo <wahyusaputropratjojo@gmail.com>
2026-02-02 21:29:29 +0800

    feat(component): update checkbox component and replace tailwindcss with css modules

--- components/base/checkbox.tsx
+++ components/base/checkbox.tsx
@@ -1,6 +1,6 @@
 import { Checkbox as CheckboxPrimitive } from "@base-ui/react/checkbox";
 import { CheckIcon } from "lucide-react";
-import { cn } from "@/lib/utils/tailwind-variants";
+import styles from "./checkbox.module.css";
 
 export function Checkbox({
   className,
@@ -8,14 +8,15 @@ export function Checkbox({
 }: CheckboxPrimitive.Root.Props) {
   return (
     <CheckboxPrimitive.Root
-      className={cn(
-        "flex size-5 cursor-pointer items-center justify-center rounded-md bg-neutral-3 focus-visible:outline-3 focus-visible:outline-primary-8 focus-visible:outline-offset-2 data-disabled:cursor-not-allowed data-readonly:cursor-default data-disabled:select-none data-checked:bg-primary-9 data-disabled:opacity-50",
-        className,
-      )}
+      className={styles.root}
+      data-slot="checkbox"
       {...props}
     >
-      <CheckboxPrimitive.Indicator>
-        <CheckIcon className="size-3.5 shrink-0 stroke-2 text-white" />
+      <CheckboxPrimitive.Indicator
+        className={styles.indicator}
+        data-slot="checkbox-indicator"
+      >
+        <CheckIcon />
       </CheckboxPrimitive.Indicator>
     </CheckboxPrimitive.Root>
   );
